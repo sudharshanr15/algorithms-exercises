6e3e3cae7d428f1cc24021d0aebf5c94819ac2b8
Sudharshan <sudharshanrms2004@gmail.com>
2024-11-05 22:36:05 +0530

    Linked list

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -1,21 +1,21 @@
 /*
   LinkedList
-  
+
   Name your class / constructor (something you can call new on) LinkedList
-  
+
   LinkedList is made by making nodes that have two properties, the value that's being stored and a pointer to
   the next node in the list. The LinkedList then keep track of the head and usually the tail (I would suggest
   keeping track of the tail because it makes pop really easy.) As you may have notice, the unit tests are the
   same as the ArrayList; the interface of the two are exactly the same and should make no difference to the
   consumer of the data structure.
-  
+
   length - integer  - How many elements in the list
   push   - function - accepts a value and adds to the end of the list
   pop    - function - removes the last value in the list and returns it
   get    - function - accepts an index and returns the value at that position
-  delete - function - accepts an index, removes value from list, collapses, 
+  delete - function - accepts an index, removes value from list, collapses,
                       and returns removed value
-                      
+
   I would suggest making a second class, a Node class. However that's up to you how you implement it. A Node
   has two properties, value and next.
 
@@ -29,53 +29,80 @@ class LinkedList {
     this.tail = null;
     this.length = 0;
   }
+
   push(value) {
-    const node = new Node(value);
-    this.length++;
-    if (!this.head) {
-      this.head = node;
+    const new_node = new Node(value);
+    if (this.head == null) {
+      this.head = new_node;
     } else {
-      this.tail.next = node;
+      this.tail.next = new_node;
     }
-    this.tail = node;
+    this.tail = new_node;
+
+    this.length++;
   }
+
   pop() {
-    return this.delete(this.length - 1);
-  }
-  _find(index) {
-    if (index >= this.length) return null;
-    let current = this.head;
-    for (let i = 0; i < index; i++) {
-      current = current.next;
-    }
-
-    return current;
-  }
-  get(index) {
-    const node = this._find(index);
-    if (!node) return void 0;
-    return node.value;
-  }
-  delete(index) {
-    if (index === 0) {
-      const head = this.head;
-      if (head) {
-        this.head = head.next;
-      } else {
-        this.head = null;
-        this.tail = null;
-      }
-      this.length--;
-      return head.value;
-    }
-
-    const node = this._find(index - 1);
-    const excise = node.next;
-    if (!excise) return null;
-    node.next = excise.next;
-    if (!node.next) this.tail = node.next;
+    const last_ele_val = this.tail.value;
     this.length--;
-    return excise.value;
+
+    let head = this.head;
+    let prev = null;
+
+    while (head.next != null) {
+      prev = head;
+      head = head.next;
+    }
+
+    delete this.tail;
+    this.tail = prev;
+    this.tail.next = null;
+
+    return last_ele_val;
+  }
+
+  get(index) {
+    let head = this.head;
+
+    let i = 0;
+    while (head != null) {
+      if (i == index) {
+        return head.value;
+      }
+      i++;
+      head = head.next;
+    }
+
+    return null;
+  }
+
+  delete(index) {
+    let i = 0;
+    let head = this.head;
+    let prev = null;
+
+    while (head != null) {
+      if (i == index) {
+        break;
+      }
+
+      i++;
+      prev = head;
+      head = head.next;
+    }
+
+    if (head == null) {
+      return null;
+    }
+
+    if (prev == null) {
+      this.head = head.next;
+    } else {
+      prev.next = head.next;
+    }
+
+    this.length--;
+    return head.value;
   }
 }
 
